eff8ce556550015cf7446b3be76dbd835a17c85d
Victoria Do <veeqdee@gmail.com>
2015-04-15 14:26:46 -0700

    Initial commit with instructions on setting up

---
+++ Gruntfile.js
@@ -0,0 +1,108 @@
+'use strict';
+
+/**
+ * Grunt Module
+ */
+module.exports = function(grunt) {
+
+  /**
+   * Load Grunt plugins
+   */
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);  
+ 
+	/**
+	 * Configuration
+	 */
+	grunt.initConfig({
+		/**
+		 * Get package meta data
+		 */
+		pkg: grunt.file.readJSON('package.json'),
+
+		/**
+		 * Set project object
+		 */
+		project: {
+		  src: {
+        root: 'src',
+        assets: 'src/assets',
+        css: ['<%= project.src.assets %>/scss'],
+        js: ['src/js']
+      },
+      dest: {
+        root: 'dist',
+        assets: 'dist/assets',
+        css: ['<%= project.dest.assets %>/css'],
+        js: ['dist/js']
+      }
+		},		
+
+    /**
+     * Sass
+     */
+    sass: {
+      dev: {
+        options: {
+          style: 'expanded',
+          compass: true
+        },
+        files: {
+          '<%= project.dest.assets %>/css/style.css': '<%= project.src.css %>/style.scss'
+        }
+      },
+      dist: {
+        options: {
+          style: 'compressed',
+          compass: true
+        },
+        files: {
+          '<%= project.dest.assets %>/css/style.css': '<%= project.src.css %>'
+        }
+      }
+    },    
+
+    /**
+     * Watch
+     */
+    watch: {
+      sass: {
+        files: '<%= project.src.css %>/{,*/}*.{scss,sass}',
+        tasks: ['sass:dev']
+      },
+
+      // I think this is buggy, but it kind of works
+      includes: {
+        files: '**/*.html',
+        tasks: ['includes:build']        
+      }
+    },   
+
+
+    // Build the site using grunt-includes
+    includes: {
+      build: {
+        cwd: 'src',
+        src: [ '*.html', 'pages/*.html' ],
+        dest: 'dist/',
+        options: {
+          flatten: true,
+          includePath: 'src/layout',
+          banner: '<!-- Spring 2015 - CSE 134B -->\n'
+        }
+      }
+    }
+
+  });
+
+  /**
+   * Default task
+   * Run `grunt` on the command line
+   */
+  grunt.registerTask('default', [
+    'sass:dev',
+    //'includes',
+    'watch'
+  ]);
+
+  grunt.loadNpmTasks('grunt-includes');  
+};
---
+++ README.md
@@ -0,0 +1,13 @@
+# Coin & Bullion Investing App
+
+## Team 
+Andrew Wilson, Danny Hsiung, Eric Fonseca, Victoria Do, Nate Bradbury
+
+## Setting up the project
+1. Install [NodeJS](https://nodejs.org/download/).
+2. Clone the respository. 
+3. Go to the root directory and type 
+        npm install
+        npm start
+4. Open your browser and navigate to **localhost:8080**. 
+5. You should see a basic webpage.
