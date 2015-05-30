54d7f6d10066955d67adcd995982e2d58e422b0f
Eric Fonseca <efonseca@ucsd.edu>
2015-05-30 16:57:03 -0700

    working on implementing back end

--- src/js/view/add_item.js
+++ src/js/view/add_item.js
@@ -1,5 +1,9 @@
 define(['vendor/tpl!../../templates/add_item.html', 'app'], function (template) {
   var AddItemPanel = Backbone.View.extend({
+    events: {
+      'click: .submit-btn' : 'sendToParse'
+    },
+
     template: template,
     id: "add",
 
@@ -10,10 +14,24 @@ define(['vendor/tpl!../../templates/add_item.html', 'app'], function (template) 
       console.log(this.options);
     },
 
+    sendToParse: function() {
+      console.log('Sending to parse!');
+      /*
+      Parse.initialize("pgIVxlWiJTswWbYnHqclimNwHZwdShkL48VmHZ8G", "Km1O6v0inoToEdisAMV80HoxEKIMwMUB3Yt5G1TG");
+      var TestObject = Parse.Object.extend("TestObject");
+      var testObject = new TestObject();
+      testObject.save({foo: "bar"}).then(function(object) {
+        alert("yay! it worked");
+      });
+      */
+    },
+
     render: function() {
       this.$el.html(this.template({
           // hardcoded attrs
       }));
+
+      //$(".submit-btn").addEventListener("click", sendToParse);
       return this;
     },
 
